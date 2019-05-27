139f5017342bf9a7ab7cbe4f4af958342179913a
Shalini <shalinin1705@gmail.com>
2019-05-27 09:03:15 +0530

    Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,5 @@ class HelloWorld
     public static void main(String args[]) 
     { 
         System.out.println("Hello, World. Happy Monday"); 
-        System.out.println("Quote for the day - U R Special"); 
     } 
 } 
