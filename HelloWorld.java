42c49dd5fb68a31cfec8b7a9d041cb0f8020e434
SHALGitHub <50937416+SHALGitHub@users.noreply.github.com>
2019-05-27 10:21:16 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ class HelloWorld
     public static void main(String args[]) 
     { 
         System.out.println("Hello, World. Happy Monday"); 
+        System.out.println("Have a good day!"); 
     } 
 } 
